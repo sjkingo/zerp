8ef94310a037e59630dbc2cf84b6b6e34affd808
Sam Kingston <sam@sjkwi.com.au>
2010-05-05 19:55:47 +1000

    allow bytecode to be sent via stdin

--- vm/main.py
+++ vm/main.py
@@ -2,22 +2,33 @@
 
 from __future__ import print_function
 from optparse import OptionParser
+import sys
 
 from exc import *
 import machine
 
 def parse_args():
-    parser = OptionParser(usage='usage: %prog [options] filename')
+    parser = OptionParser(usage='usage: %prog [options] [filename]')
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
             default=False, help='Be verbose in parsing and executing code')
     options, args = parser.parse_args()
-    if len(args) != 1:
+
+    filename = None
+    if len(args) == 0:
+        filename = '<stdin>'
+    elif len(args) == 1:
+        filename = args[0]
+    else:
         parser.error('incorrect number of parameters')
-    return (options, args[0])
+
+    return (options, filename)
 
 def parse_program(filename):
-    with open(filename, 'r') as fp:
-        lines = fp.readlines()
+    if filename == '<stdin>':
+        lines = sys.stdin.readlines()
+    else:
+        with open(filename, 'r') as fp:
+            lines = fp.readlines()
     parsed = []
     for l in lines:
         l = l.strip()
