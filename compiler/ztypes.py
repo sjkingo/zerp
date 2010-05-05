77384eb0d89be502622f408c1455c14dee95d162
Sam Kingston <sam@sjkwi.com.au>
2010-05-06 09:42:35 +1000

    types

---
+++ compiler/ztypes.py
@@ -0,0 +1,28 @@
+class ZType(object):
+    type_name = 'unknown'
+    val = None
+    id = None
+
+    def __str__(self):
+        return str('<%s(%s)>' % (self.type_name, self.id))
+
+    def __repr__(self):
+        return repr(self.val)
+
+class Integer(ZType):
+    type_name = 'integer'
+
+    def __init__(self, id, val=None):
+        self.id = id
+        if val is not None:
+            self.set(val)
+
+    def __eq__(self, other):
+        return self.val == other
+
+    def set(self, val):
+        self.val = int(val)
+
+known_types = {
+    'integer': Integer,
+}
